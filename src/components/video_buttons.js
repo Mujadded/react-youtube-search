de2a0b0e5ee7bfd62eb23fcf342747d1e3ffe84a
Mujadded <mujadded.alif@welltravel.com>
2018-08-21 19:13:53 +0600

    video buttons added

---
+++ src/components/video_buttons.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Files from "react-files";
+
+const VideoButtons = ({onNextClick, onExportClick, fileReader}) => {
+  return (
+    <div className="video-buttons">
+        <button className="btn" onClick={ () => onNextClick() }> Play next in queue </button>
+        <a className="btn btn-primary" id="link" download="queue.json" onClick={ () => onExportClick() }> Export queue </a>
+        <div className="files btn btn-warning">
+          <Files
+            className="files-dropzone"
+            onChange={file => {
+              fileReader.readAsText(file[0]);
+            }}
+            onError={err => console.log(err)}
+            accepts={[".json"]}
+            multiple={false}
+            clickable >
+            Import queue
+          </Files>
+        </div>
+      </div>
+  );
+}
+
+export default VideoButtons;
